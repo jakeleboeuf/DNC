76e9db0a9594f7be27a7678d3e73c836ca4d964d
Jake LeBoeuf <dev@jakeleboeuf.com>
2012-08-23 10:23:11 -0400

    updated socail css

--- index.php
+++ index.php
@@ -178,7 +178,7 @@ if( !empty($favicon) )
 </ul>
 
 <div class="social">
-<div style="display:block; margin-top:-1px;float:left;" class="fb-like" data-href="https://www.datenightpdx.org" data-send="false" data-layout="button_count" data-width="100" data-show-faces="true"></div>
+<div style="margin-right: 15px; display:block; margin-top:-1px;float:left;" class="fb-like" data-href="https://www.datenightpdx.org" data-send="false" data-layout="button_count" data-width="100" data-show-faces="true"></div>
     <div style="display:block; margin-top:-1px;float:left;"><a href="https://twitter.com/datenightpdx" class="twitter-follow-button" data-show-count="true" data-lang="en" data-show-screen-name="false">Follow @datenightpdx</a></div>
     <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 </div>
